81284ff788cebbe00adb931ae79fc1e87f895494
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-06-09 23:27:42 +0300

    Added .gitignore file and put .github/workflows inside it; added an implementation of of the bubble_sort side by side built in sort method

--- main.rb
+++ main.rb
@@ -5,10 +5,9 @@ p bubble_sort(array) { |first_number, second_number| (first_number - second_numb
 array = [4, 7,	2,	1,	10,	8,	3,	5,	6,	9]
 p bubble_sort(array) { |first_number, second_number| (first_number - second_number).positive? }
 
-p(bubble_sort_by(%w[hi hello hey]) do |left, right|
-  (left.length - right.length).positive?
-end)
+unsorted = (0..10).to_a.reverse!
+p bubble_sort(unsorted) { |first_number, second_number| (first_number - second_number).positive? } == unsorted.sort
 
-p(bubble_sort_by(%w[hi hello hey h]) do |left, right|
+p(bubble_sort_by(%w[hi hello hey]) do |left, right|
   (left.length - right.length).positive?
 end)
