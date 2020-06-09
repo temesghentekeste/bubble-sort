de71cc930e61ef78954fb98ebcfb10bf3100b48b
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-06-09 23:31:31 +0300

    Fixed Rubocop Error Report

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative('bubble_sort')
 array = [2, 0, 5, 3]
 p bubble_sort(array) { |first_number, second_number| (first_number - second_number).positive? }
 
-array = [4, 7,	2,	1,	10,	8,	3,	5,	6,	9]
+array = [4, 7, 2,	1, 10, 8,	3, 5,	6, 9]
 p bubble_sort(array) { |first_number, second_number| (first_number - second_number).positive? }
 
 unsorted = (0..10).to_a.reverse!
